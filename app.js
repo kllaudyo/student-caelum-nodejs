f45154c80563904a9e444fb807b19b7cd268532a
Claudio Acioli <kllaudyo@gmail.com>
2017-09-17 13:00:18 -0300

    implementando ejs para separar views

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var express = require('express');
 //chama a função para conseguir trabalhar com o modulo
 var app = express();
 
+//injetando embeded javascript no express
+app.set('view engine', 'ejs');
+
 //configurando o metodo get para url /produtos
 app.get("/produtos", function(request, response){
     //objeto response do express, metodo send que envia
     //retorno para o cliente
-    response.send("<html><body><h1>Listagem de Produtos</h1></body></html>");
+    //response.send("<html><body><h1>Listagem de Produtos</h1></body></html>");
+    response.render("produtos/lista");
 });
 
 app.listen(3000, function () {
