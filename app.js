2e00a78c8f142bb78df75537395ad0b659b7cace
Claudio Acioli <kllaudyo@gmail.com>
2017-09-17 11:47:57 -0300

    Exemplo simples usando express

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by claudio on 17/09/17.
+ */
+
+//importa modulo express baixado com sudo npm install express -save
+var express = require('express');
+//chama a função para conseguir trabalhar com o modulo
+var app = express();
+
+//configurando o metodo get para url /produtos
+app.get("/produtos", function(request, response){
+    //objeto response do express, metodo send que envia
+    //retorno para o cliente
+    response.send("<html><body><h1>Listagem de Produtos</h1></body></html>");
+});
+
+app.listen(3000, function () {
+    console.log("Servidor rodando");
+});
