e75baea5ba39c9064807f05088ff2c7ded6c52b5
Claudio Acioli <kllaudyo@gmail.com>
2017-09-18 21:22:15 -0300

    criando um módulo usando commandJS

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 /**
  * Created by claudio on 17/09/17.
  */
+//importando um arquivo meu
+// ./ signinfica apartir da raiz do projeto
 
-//importa modulo express baixado com sudo npm install express -save
-var express = require('express');
-//chama a função para conseguir trabalhar com o modulo
-var app = express();
-
-//injetando embeded javascript no express
-app.set('view engine', 'ejs');
+var app = require('./config/express')();
 
 //configurando o metodo get para url /produtos
 app.get("/produtos", function(request, response){
     //objeto response do express, metodo send que envia
     //retorno para o cliente
     //response.send("<html><body><h1>Listagem de Produtos</h1></body></html>");
+    console.log("listando produto");
     response.render("produtos/lista");
 });
 
