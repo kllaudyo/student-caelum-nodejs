3c6ecb299b0643040bc08b52caa1532090b09c91
Claudio Acioli <kllaudyo@gmail.com>
2017-09-18 22:23:42 -0300

    Adicionando Driver Mysql ao projeto e criando a primeira consulta, usando callback e retornando json

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,6 +4,27 @@
 
 module.exports = function(app){
     app.get('/produtos', function(request, response){
-        response.render('produtos/lista');
+
+        //objeto driver do mysql
+        var mysql = require('mysql');
+
+        //criando a conexão mysql
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'root',
+            database : 'casadocodigo'
+        });
+
+        connection.query('select * from livros', function (err, result) {
+            if(err){
+                console.log('erro',err);
+            }else{
+                response.send(result);
+            }
+        });
+
+        connection.end();
+
     });
 };
