4eafdd926ffbfb9d06137f4481cf76de70f1037f
Claudio Acioli <kllaudyo@gmail.com>
2017-09-18 21:45:30 -0300

    Organizando, colocando pasta views dentro de app e definindo diretorio views do express

--- config/express.js
+++ config/express.js
@@ -1,5 +1,8 @@
 var app = require('express')();
+    //setando a engine para ejs;
     app.set('view engine', 'ejs');
+    //setando o diretório de vies do ejs
+    app.set('views', './app/views');
 
 module.exports = function () {
     return app;
