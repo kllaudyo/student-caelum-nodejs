7f475792335024014423fc7cf57a6a77217b6fab
Claudio Acioli <kllaudyo@gmail.com>
2017-09-20 22:06:01 -0300

    Adicionando e usando a biblioteca de testes, supertest, para simplificar a escrita do teste

--- test/produtos.js
+++ test/produtos.js
@@ -2,30 +2,16 @@
  * Created by claudio on 20/09/17.
  */
 
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('#ProdutosController', function(){
 
     it('#listagem json', function(done){
-
-        var request = {
-            hostname:'localhost',
-            port:3000,
-            path:'/produtos',
-            headers:{
-                'Accept':'application/json'
-            }
-        };
-
-        http.get(request, function(response){
-
-            assert.equal(response.statusCode, 302);
-            assert.equal(response.headers['content-type'], 'application/json; charset=utf-8');
-            done();
-
-        });
-
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 
 });
