b133bf6de402505db1da5826d2cafc4ac0d18227
Claudio Acioli <kllaudyo@gmail.com>
2017-09-22 12:05:08 -0300

    Preparando cadastro de promocoes

---
+++ app/routes/promocoes.js
@@ -0,0 +1,23 @@
+/**
+ * Created by claudio on 22/09/17.
+ */
+module.exports = function(app){
+    app.get('/promocoes/form', function (request, response, next) {
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(err, result){
+            if(err){
+                return next(err);
+            }else{
+                response.render('promocoes/form',{livros:result});
+            }
+        });
+        connection.end();
+    });
+
+    app.post('/promocoes',function (request, response, next) {
+        var promocoes = request.body;
+        console.log(promocoes);
+        response.redirect('promocoes/form');
+    });
+};
